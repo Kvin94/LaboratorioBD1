a89fb513cfb4718192a7f1fda82d7c5b1fe91b6e
Kvin94 <kvinramos94@gmail.com>
2024-04-23 21:52:09 -0600

    guia4_bd

---
+++ guia4/consultas.sql
@@ -0,0 +1,22 @@
+      
+    -- Consultas 
+    SELECT * FROM biblioteca.municipios;
+    SELECT * FROM biblioteca.departamentos;
+    SELECT * FROM biblioteca.distritos;
+    SELECT * FROM biblioteca.direcciones;
+    SELECT * FROM biblioteca.cargos;
+    SELECT * FROM biblioteca.empleados;
+    SELECT * FROM biblioteca.lectores;
+    SELECT * FROM biblioteca.historialprestamos;
+    SELECT * FROM biblioteca.prestamos;
+    SELECT * FROM biblioteca.moras;
+    SELECT * FROM biblioteca.autores;
+    SELECT * FROM biblioteca.categorialibros;
+    SELECT * FROM biblioteca.editoriales;
+    SELECT * FROM biblioteca.condicionlibros;
+    SELECT * FROM biblioteca.libros;
+    SELECT * FROM biblioteca.prestamolibro;
+    SELECT * FROM biblioteca.autoreslibros;
+    
+    
+    
